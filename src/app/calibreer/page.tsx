8c65ea925f3e037b0465f91a916b7bf39c62aab7
Thijmen Kurk <tkurk@suneco.nl>
2024-03-04 16:56:13 +0100

    eggsLeft tracker

---
+++ src/app/calibreer/page.tsx
@@ -0,0 +1,39 @@
+import { getEggsLeft, setEggsLeft } from "@/actions";
+import { SubmitButton } from "../_components/submit-button";
+
+const locale = "nl-NL";
+
+export default async function Page() {
+  const eggsLeft = await getEggsLeft();
+
+  const date = new Date();
+
+  return (
+    <main className="flex flex-col items-center justify-between">
+      <div className="flex flex-col gap-4 items-center justify-center">
+        <div className="flex gap-4">
+          <div className="flex flex-col gap-5 items-center">
+            <h1 className="text-6xl font-bold">
+              {date.toLocaleString(locale, { weekday: "long" })}
+            </h1>
+            <h2 className="text-xl">{date.toLocaleDateString(locale)}</h2>
+            <p>er zijn nog {eggsLeft} eitjes over.</p>
+
+            <div className="flex gap-2">
+              <form action={setEggsLeft}>
+                <input
+                  type="number"
+                  name="count"
+                  min="0"
+                  max="100"
+                  defaultValue={eggsLeft} />
+                <SubmitButton>Opslaan</SubmitButton>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+
+  )
+}
